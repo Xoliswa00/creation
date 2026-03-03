c6e1349c47bcda846a7fe16d734b3efc00c152c8
Xoliswa00 <164828712+Xoliswa00@users.noreply.github.com>
2026-03-03 02:47:06 +0200

    Initial Laravel Billing System Setup

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Billing System Dashboard
+        </h2>
+    </x-slot>
+
+    <div class="p-6">
+        Welcome to Xquisite Billing Core.
+    </div>
+</x-app-layout>
